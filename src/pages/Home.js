0f49c45da4904b4b90eb1ebfd874f9770c655783
Hélio Neto <helioneto29@hotmail.com>
2021-02-25 11:11:43 -0300

    Animação do background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,51 @@
-import React from "react";
-import {useStyles} from './HomeStyles'
+import React, {useState} from "react";
+import {useStyles} from './HomeStyles';
+import Background2 from "./GradienteHome.svg";
 
 function Home() {
+
+  var x = -300;
+  const [xx, setXX] = useState();
+
+  function rodar1(){
+      x=-300;
+      setXX(x);
+  }
+
+  function rodar2(){
+      x=-600;
+      setXX(x);
+  }
+
+  function rodar3(){
+      x=-900;
+      setXX(x);
+  }
+
+  function rodar4(){
+      x=-1300;
+      setXX(x);
+  }
+
   const classes = useStyles();
   return(
-    <div className={classes.homeContainer}>
-      Home
+    <div className={classes.homeContainer} style={{backgroundImage: "url("+ Background2 +")", height: "100vh"}}>
+      <div className={classes.homeContainerChildren} style={{backgroundPositionX: xx, backgroundPositionY: -200, height: "100vh"}}>
+        <div className={classes.homeGeneral}>
+          <div className={classes.button1}>
+            <button onClick={rodar1} style={{width:"10vw", height:"5vh"}}>1</button>
+          </div>
+          <div className={classes.button2}>
+            <button onClick={rodar2} style={{width:"10vw", height:"5vh"}}>2</button>
+          </div>
+          <div className={classes.button3}>
+            <button onClick={rodar3} style={{width:"10vw", height:"5vh"}}>3</button>
+          </div>
+          <div className={classes.button4}>
+            <button onClick={rodar4} style={{width:"10vw", height:"5vh"}}>4</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/HomeStyles.js
+++ src/pages/HomeStyles.js
@@ -1,8 +1,37 @@
 import { makeStyles } from "@material-ui/core";
+import Background from "./ImagemDeFundo.svg";
 
 export const useStyles = makeStyles({
   homeContainer: {
     width: '100%',
     height: '100%',
-  }
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundSize: '1700px',
+  },
+  homeContainerChildren: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundImage: "url("+ Background +")",
+    backgroundSize: '1700px',
+  },
+  homeGeneral: {
+    display: 'flex',
+    paddingTop: '50vh',
+    alignSelf: 'center',
+  },
+  button1: {
+    paddingRight: '10vw',
+  },
+  button2: {
+    paddingRight: '5vw',
+  },
+  button3: {
+    paddingLeft: '5vw',
+  },
+  button4: {
+    paddingLeft: '10vw',
+  },
 });
